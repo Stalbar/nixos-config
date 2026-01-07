bc7ea98ff609fa4088c0bbc12491afddb873a017
stalbar <aleksey.smorshyok@gmail.com>
2026-01-07 13:54:54 +0300

    Initial NixOS configuration

---
+++ configuration.nix
@@ -0,0 +1,42 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos"; # Define your hostname.
+
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Minsk";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  users.users.stalbar = {
+    isNormalUser = true;
+    description = "aleksey";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+  ];
+
+  system.stateVersion = "25.11"; 
+
+}

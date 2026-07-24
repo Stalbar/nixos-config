a5fc866938cf15ff57d5844d080cc7d8c10636a9
stalbar <aleksey.smorshyok@gmail.com>
2026-07-24 10:26:52 +0300

    fix(hyprland): switch configType to hyprlang for native reliable keybindings and workspace rules

--- home/stalbar/programs/hyprland.nix
+++ home/stalbar/programs/hyprland.nix
@@ -6,154 +6,122 @@ in
 {
   wayland.windowManager.hyprland = {
     enable = true;
-    configType = "lua";
+    configType = "hyprlang";
     settings = {
-      mainMod = {
-        _var = "SUPER";
-        name = "mainMod";
-      };
-
-      terminal = {
-        _var = "foot";
-      };
-
-      browser = {
-        _var = "zen";
-      };
-
-      fileManager = {
-        _var = "thunar";
-      };
-
-      launcher = {
-        _var = "qs-app-launcher";
-      };
-
-      powerMenu = {
-        _var = "qs-power-menu";
-        name = "powerMenu";
-      };
+      "$mainMod" = "SUPER";
 
       monitor = [
-        {
-          output = "HDMI-A-1";
-          mode = "highres";
-          position = "auto";
-          scale = 1.25;
-        }
-        {
-          output = "eDP-1";
-          mode = "highrr";
-          position = "0x0";
-          scale = 1;
-        }
-        {
-          output = "";
-          mode = "preferred";
-          position = "auto";
-          scale = 1;
-        }
+        "HDMI-A-1, highres, auto, 1.25"
+        "eDP-1, highrr, 0x0, 1"
+        ", preferred, auto, 1"
       ];
 
-      config = {
-        cursor = {
-          no_hardware_cursors = true;
-        };
+      workspace = [
+        "1, monitor:HDMI-A-1, default:true"
+        "2, monitor:HDMI-A-1"
+        "3, monitor:HDMI-A-1"
+        "4, monitor:HDMI-A-1"
+        "5, monitor:HDMI-A-1"
+        "11, monitor:eDP-1, default:true"
+      ];
 
-        input = {
-          kb_layout = "us,ru";
-          kb_options = "grp:win_space_toggle";
-          follow_mouse = 1;
-          sensitivity = 0;
+      cursor = {
+        no_hardware_cursors = true;
+      };
 
-          touchpad = {
-            natural_scroll = true;
-            middle_button_emulation = true;
-          };
-        };
+      input = {
+        kb_layout = "us,ru";
+        kb_options = "grp:win_space_toggle";
+        follow_mouse = 1;
+        sensitivity = 0;
 
-        general = {
-          gaps_in = 5;
-          gaps_out = 10;
-          border_size = 2;
-          resize_on_border = true;
-          layout = "dwindle";
-
-          col = {
-            active_border = {
-              colors = [
-                "rgba(7dcfffee)"
-                "rgba(bb9af7cc)"
-              ];
-              angle = 45;
-            };
-            inactive_border = "rgba(1a1b2655)";
-          };
-        };
-
-        decoration = {
-          rounding = 12;
-          active_opacity = 1.0;
-          inactive_opacity = 1.0;
-          dim_inactive = false;
-
-          blur = {
-            enabled = true;
-            size = 10;
-            passes = 3;
-            new_optimizations = true;
-            popups = true;
-          };
-
-          shadow = {
-            enabled = false;
-          };
-        };
-
-        dwindle = {
-          preserve_split = true;
-        };
-
-        misc = {
-          disable_hyprland_logo = true;
-          disable_splash_rendering = true;
+        touchpad = {
+          natural_scroll = true;
+          middle_button_emulation = true;
         };
       };
 
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+        resize_on_border = true;
+        layout = "dwindle";
+
+        "col.active_border" = "rgba(7dcfffee) rgba(bb9af7cc) 45deg";
+        "col.inactive_border" = "rgba(1a1b2655)";
+      };
+
+      decoration = {
+        rounding = 12;
+        active_opacity = 1.0;
+        inactive_opacity = 1.0;
+        dim_inactive = false;
+
+        blur = {
+          enabled = true;
+          size = 10;
+          passes = 3;
+          new_optimizations = true;
+          popups = true;
+        };
+
+        shadow = {
+          enabled = false;
+        };
+      };
+
+      dwindle = {
+        preserve_split = true;
+      };
+
+      misc = {
+        disable_hyprland_logo = true;
+        disable_splash_rendering = true;
+      };
+
+      exec-once = [
+        "change-wallpaper --reapply"
+        "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"
+        "nm-applet --indicator"
+        "blueman-applet"
+        "mkdir -p $HOME/Pictures/Screenshots"
+      ];
+
       bind = [
-        "SUPER, Q, exec, foot"
-        "SUPER, F4, killactive,"
-        "SUPER, V, togglefloating,"
+        "$mainMod, Q, exec, foot"
+        "$mainMod, F4, killactive,"
+        "$mainMod, V, togglefloating,"
         "ALT, SPACE, exec, qs-app-launcher"
-        "SUPER, SPACE, exec, qs-app-launcher"
-        "SUPER, M, exec, qs-power-menu"
-        "SUPER, N, exec, qs-action-center"
-        "SUPER, R, exec, change-wallpaper"
-        "SUPER, F, fullscreen, 0"
+        "$mainMod, SPACE, exec, qs-app-launcher"
+        "$mainMod, M, exec, qs-power-menu"
+        "$mainMod, N, exec, qs-action-center"
+        "$mainMod, R, exec, change-wallpaper"
+        "$mainMod, F, fullscreen, 0"
 
-        "SUPER, H, movefocus, l"
-        "SUPER, L, movefocus, r"
-        "SUPER, K, movefocus, u"
-        "SUPER, J, movefocus, d"
+        "$mainMod, H, movefocus, l"
+        "$mainMod, L, movefocus, r"
+        "$mainMod, K, movefocus, u"
+        "$mainMod, J, movefocus, d"
 
-        "SUPER ALT, H, swapwindow, l"
-        "SUPER ALT, L, swapwindow, r"
-        "SUPER ALT, K, swapwindow, u"
-        "SUPER ALT, J, swapwindow, d"
+        "$mainMod ALT, H, swapwindow, l"
+        "$mainMod ALT, L, swapwindow, r"
+        "$mainMod ALT, K, swapwindow, u"
+        "$mainMod ALT, J, swapwindow, d"
 
-        "SUPER, 1, workspace, 1"
-        "SUPER, 2, workspace, 2"
-        "SUPER, 3, workspace, 3"
-        "SUPER, 4, workspace, 4"
-        "SUPER, 5, workspace, 5"
-        "SUPER, 0, workspace, 11"
+        "$mainMod, 1, workspace, 1"
+        "$mainMod, 2, workspace, 2"
+        "$mainMod, 3, workspace, 3"
+        "$mainMod, 4, workspace, 4"
+        "$mainMod, 5, workspace, 5"
+        "$mainMod, 0, workspace, 11"
 
-        "SUPER ALT, 1, movetoworkspace, 1"
-        "SUPER ALT, 2, movetoworkspace, 2"
-        "SUPER ALT, 3, movetoworkspace, 3"
-        "SUPER ALT, 4, movetoworkspace, 4"
-        "SUPER ALT, 5, movetoworkspace, 5"
-        "SUPER ALT, 0, movetoworkspace, 11"
+        "$mainMod ALT, 1, movetoworkspace, 1"
+        "$mainMod ALT, 2, movetoworkspace, 2"
+        "$mainMod ALT, 3, movetoworkspace, 3"
+        "$mainMod ALT, 4, movetoworkspace, 4"
+        "$mainMod ALT, 5, movetoworkspace, 5"
+        "$mainMod ALT, 0, movetoworkspace, 11"
 
         "ALT, N, exec, neovide --no-fork"
         "ALT, F, exec, zen"
@@ -162,7 +130,7 @@ in
 
         # Screenshots
         ", Print, exec, grimblast --notify copysave area ~/Pictures/Screenshots/screenshot.png"
-        "SUPER, S, exec, grim -g \"$(slurp)\" - | swappy -f -"
+        "$mainMod, S, exec, grim -g \"$(slurp)\" - | swappy -f -"
       ];
 
       binde = [
@@ -175,29 +143,6 @@ in
       bindl = [
         ", XF86AudioMute, exec, volume --mute-volume"
         ", XF86AudioMicMute, exec, volume --mute-mic"
-      ];
-
-      on = [
-        {
-          _args = [
-            "hyprland.start"
-            (lib.generators.mkLuaInline ''
-              function()
-                hl.exec_cmd("change-wallpaper --reapply")
-                hl.exec_cmd(${builtins.toJSON "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1"})
-                hl.exec_cmd("nm-applet --indicator")
-                hl.exec_cmd("blueman-applet")
-                hl.exec_cmd("mkdir -p $HOME/Pictures/Screenshots")
-                hl.exec_cmd("hyprctl keyword workspace 1,monitor:HDMI-A-1,default:true")
-                hl.exec_cmd("hyprctl keyword workspace 2,monitor:HDMI-A-1")
-                hl.exec_cmd("hyprctl keyword workspace 3,monitor:HDMI-A-1")
-                hl.exec_cmd("hyprctl keyword workspace 4,monitor:HDMI-A-1")
-                hl.exec_cmd("hyprctl keyword workspace 5,monitor:HDMI-A-1")
-                hl.exec_cmd("hyprctl keyword workspace 11,monitor:eDP-1,default:true")
-              end
-            '')
-          ];
-        }
       ];
     };
   };
